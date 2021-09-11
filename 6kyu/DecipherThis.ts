6a06680563f58515f2f9f54324f6f1f2a33f1841
ahonore42 <a1honore80@gmail.com>
2021-09-11 15:36:58 -0700

    add 6kyu solution for Decipher This

--- 6kyu/DecipherThis.ts
+++ 6kyu/DecipherThis.ts
@@ -10,3 +10,17 @@
 
 // decipherThis('72olle 103doo 100ya'); // 'Hello good day'
 // decipherThis('82yade 115te 103o'); // 'Ready set go'
+
+export function decipherThis(str: string): string {
+  return str.split(' ').map((word, idx) => {
+    const converted: string = String.fromCharCode(Number(word.match(/[0-9]+/g)));
+    const letters: any = word.match(/[a-zA-Z]+/g);
+    if (!letters) word = converted;
+    else {
+      if (letters[0].length === 1) word = converted + letters[0];
+      else if (letters[0].length === 2) word = converted + letters[0].slice(-1) + letters[0][0];
+      else word = converted + letters[0].slice(-1) + letters[0].slice(1,-1) + letters[0][0];
+    }
+    return word;
+  }).join(' ');
+}
